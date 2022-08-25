39f11a82e7b2443a901ecc80365c24c7dd595ca4
Artun Kara <artunankara@gmail.com>
2022-08-25 12:52:12 +0300

    Movie Example

---
+++ LrnMvcMovie/LrnMvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using LrnMvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LrnMvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+        public ActionResult Index(string searchString)
+        {
+            var movies = from m in db.Movies select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+            return View(db.Movies.ToList());
+           
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LrnMvcMovie/LrnMvcMovie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LrnMvcMovie.MvcApplication" Language="C#" %>
